4e9aa6eb9cb42037477c53ba1d5acee3fc9c4057
tsieprawski <tsieprawski@localhost>
2016-05-30 16:51:56 +0200

    Draft of installing cmake targets.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-astyle -q {ubjs,test}/*.{c,h} 
+astyle -q {include,src,test}/*.{c,h} 
 
+test -d dist && rm -rf dist
 test -d build && rm -rf build
+
 mkdir build
 cd build
 mkdir logs
@@ -11,7 +13,7 @@ echo "conan..."
 conan install .. &> logs/conan.txt || exit 1
 
 echo "cmake..."
-cmake -DCMAKE_BUILD_TYPE=Debug .. &> logs/cmake.txt|| exit 1
+cmake -DCMAKE_INSTALL_PREFIX:PATH=../dist -DCMAKE_BUILD_TYPE=Debug .. &> logs/cmake.txt|| exit 1
 
 echo "build..."
 make all &> logs/build.txt || exit 1
@@ -30,6 +32,9 @@ ohcount ../ubjs/*.{c,h} > logs/loc-impl.txt
 ohcount -i ../ubjs/*.{c,h} >> logs/loc-impl.txt
 ohcount ../test/*.{c,h} > logs/loc-test.txt
 ohcount -i ../test/*.{c,h} >> logs/loc-test.txt
+
+echo "dist..."
+make install/strip
 
 echo "OK!"
 cd ..
