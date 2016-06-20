f4bb51e9982b299f381eea135d02d9c0634bf7b4
tsieprawski <tsieprawski@localhost>
2016-06-20 12:48:26 +0200

    Preparing to relink to new ptrie subrepo.

--- test/test.c
+++ test/test.c
@@ -2,14 +2,12 @@
 #include "test_list.h"
 
 #include "test_common.h"
-#include "test_trie.h"
 #include "test_objects.h"
 #include "test_parser.h"
 #include "test_writer.h"
 
 void install_tests() {
     suite_common();
-    suite_trie();
     suite_objects();
     suite_parser();
     suite_writer();
