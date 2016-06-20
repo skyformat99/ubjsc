f10c3ba238b03f3a5745dc72c55c69e1b52afba0
tsieprawski <tsieprawski@localhost>
2016-06-20 12:40:35 +0200

    Preparing to extract tries and develop them in other repository.

---
+++ ptrie/test/test.c
@@ -0,0 +1,148 @@
+#include <stdlib.h>
+
+#include "test.h"
+
+CU_pSuite suite_trie() {
+    CU_pSuite suite = CU_add_suite("ptrie", 0, 0);
+
+    CU_ADD_TEST(suite, test_trie_new);
+    CU_ADD_TEST(suite, test_trie_1_item);
+
+    return suite;
+}
+
+void test_trie_new() {
+    ptrie *trie = 0;
+
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_new(0, 0));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_new(0, &trie));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_new(free, 0));
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(free, &trie));
+    CU_ASSERT_NOT_EQUAL(0, trie);
+
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_free(0));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+    CU_ASSERT_EQUAL(0, trie);
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_free(&trie));
+}
+
+void nothing(void *value) {
+}
+
+void test_trie_1_item() {
+    ptrie *trie = 0;
+    char *value = "123";
+    char *nvalue;
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "a", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_delete(trie, 1, "a"));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 10, "abcdefghij", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 10, "abcdefghij", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_delete(trie, 10, "abcdefghij"));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_get(trie, 10, "abcdefghij", &nvalue));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "a", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "b", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "c", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "b", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "c", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_delete(trie, 1, "a"));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "b", &nvalue));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "c", &nvalue));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_delete(trie, 1, "c"));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "b", &nvalue));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_get(trie, 1, "c", &nvalue));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "a", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "a", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 2, "aa", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 3, "aaa", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 2, "aa", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 3, "aaa", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 2, "aa", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 6, "aabbbb", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 14, "aabbbbcccccccc", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 2, "aa", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 6, "aabbbb", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 14, "aabbbbcccccccc", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "a", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 2, "bb", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 3, "ccc", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 2, "bb", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 3, "ccc", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "c", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "b", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "a", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "b", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "c", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+
+    printf("*******************\n\n\n");
+    CU_ASSERT_EQUAL(UR_OK, ptrie_new(nothing, &trie));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 1, "a", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 2, "ab", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 5, "abbbb", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 5, "aaaaa", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 5, "abbaa", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_put(trie, 3, "aaa", value));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 1, "a", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 2, "ab", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 3, "aaa", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 5, "abbbb", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 5, "aaaaa", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_get(trie, 5, "abbaa", &nvalue));
+    CU_ASSERT_EQUAL(nvalue, value);
+    CU_ASSERT_EQUAL(UR_OK, ptrie_delete(trie, 3, "aaa"));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_delete(trie, 3, "aaa"));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_delete(trie, 1, "a"));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_delete(trie, 1, "a"));
+    CU_ASSERT_EQUAL(UR_ERROR, ptrie_delete(trie, 3, "abb"));
+    CU_ASSERT_EQUAL(UR_OK, ptrie_free(&trie));
+}
