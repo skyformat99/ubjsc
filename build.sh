7373345370ac0696ce912cefa8a47057aa2fd1bb
tsieprawski <tsieprawski@localhost>
2016-06-23 13:16:52 +0200

    Better build artifacts.

--- build.sh
+++ build.sh
@@ -9,31 +9,28 @@ cd build
 mkdir logs
 
 echo "conan..."
-conan install .. &> logs/conan.txt || exit 1
+conan install .. &> logs/1-conan.txt || exit 1
 
 echo "cmake..."
-cmake -DCMAKE_INSTALL_PREFIX:PATH=../dist -DCMAKE_BUILD_TYPE=Debug .. &> logs/cmake.txt|| exit 1
+cmake -DCMAKE_INSTALL_PREFIX:PATH=../dist -DCMAKE_BUILD_TYPE=Debug .. &> logs/2-cmake.txt|| exit 1
 
 echo "build..."
-make all &> logs/build.txt || exit 1
+make all &> logs/3-build.txt || exit 1
 
 echo "test..."
-ctest -VV . &> logs/test.txt || exit 1
+ctest -VV . &> logs/4-test.txt || exit 1
 
 echo "coverage..."
-make coverage &> logs/coverage.txt || exit 1
+make coverage &> logs/5-coverage.txt || exit 1
 
 echo "valgrind..."
-valgrind --leak-check=full bin/unittest &> logs/valgrind.txt || exit 1
+valgrind --leak-check=full bin/unittest &> logs/6-valgrind.txt || exit 1
 
 echo "ohcount..."
-ohcount ../ubjs/*.{c,h} > logs/loc-impl.txt
-ohcount -i ../ubjs/*.{c,h} >> logs/loc-impl.txt
-ohcount ../test/*.{c,h} > logs/loc-test.txt
-ohcount -i ../test/*.{c,h} >> logs/loc-test.txt
-
-# echo "dist..."
-# make package package_source &> logs/package.txt || exit 1
+ohcount ../{src,include}/*.{c,h} > logs/7-loc-impl.txt
+ohcount -i ../{src,include}/*.{c,h} >> logs/7-loc-impl.txt
+ohcount ../test/*.{c,h} > logs/7-loc-test.txt
+ohcount -i ../test/*.{c,h} >> logs/7-loc-test.txt
 
 echo "OK!"
 cd ..
