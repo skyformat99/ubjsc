924980bffbf4103c05d428c5aaaab6c5fd2728b8
tsieprawski <tsieprawski@localhost>
2016-06-23 15:27:28 +0200

    Better artifact names. unittest binary also generates XML.

--- dist.sh
+++ dist.sh
@@ -6,7 +6,7 @@ cd build
 
 echo "packages..."
 cmake .. &> logs/8-cmake.txt|| exit 1
-make package package_source &> logs/8-make.txt || exit 1
+make package package_source &> logs/dist-make.txt || exit 1
 
 echo "OK"
 cd ..
--- misc.sh
+++ misc.sh
@@ -5,16 +5,16 @@ test -d build || exit 1
 cd build
 
 echo "coverage..."
-make coverage &> logs/test-coverage.txt || exit 1
+make coverage &> logs/misc-coverage.txt || exit 1
 
 echo "valgrind..."
-valgrind --leak-check=full bin/unittest &> logs/test-valgrind.txt || exit 1
+valgrind --leak-check=full bin/unittest &> logs/misc-valgrind.txt || exit 1
 
 echo "ohcount..."
-ohcount ../{src,include}/*.{c,h} > logs/7-loc-impl.txt
-ohcount -i ../{src,include}/*.{c,h} >> logs/7-loc-impl.txt
-ohcount ../test/*.{c,h} > logs/7-loc-test.txt
-ohcount -i ../test/*.{c,h} >> logs/7-loc-test.txt
+ohcount ../{src,include}/*.{c,h} > logs/misc-loc-impl.txt
+ohcount -i ../{src,include}/*.{c,h} >> logs/misc-loc-impl.txt
+ohcount ../test/*.{c,h} > logs/misc-loc-test.txt
+ohcount -i ../test/*.{c,h} >> logs/misc-loc-test.txt
 
 echo "OK"
 cd ..
