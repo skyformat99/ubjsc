625809d560f7096d46d784502b42439ec1d975b8
tsieprawski <tsieprawski@localhost>
2016-06-29 16:55:16 +0200

    More suites.

--- test/test_frmwrk.c
+++ test/test_frmwrk.c
@@ -35,6 +35,7 @@ struct tresults_suite {
 };
 
 struct tresults_test {
+    ttest *test;
     int failed;
     unsigned int asserts_run;
     unsigned int asserts_failed;
@@ -79,7 +80,7 @@ void tresults_test_free(tresults_test **);
 void tresults_test_add_assert(tresults_test *, tresults_assert *);
 void tresults_test_print(tresults_test *);
 
-void tresults_suite_new(tresults_suite **);
+void tresults_suite_new(tsuite *,tresults_suite **);
 void tresults_suite_free(tresults_suite **);
 void tresults_suite_add_test(tresults_suite *,tresults_test *);
 void tresults_suite_print(tresults_suite *);
@@ -92,6 +93,7 @@ void tresults_print(tresults *);
 void tsuite_new(char *,tbefore_f,tafter_f,tsuite **);
 void tsuite_free(tsuite **);
 void tsuite_add_test(tsuite *,char *,ttest_f);
+void tsuite_run(tsuite *,tresults_suite **);
 
 void tresults_assert_new(tresults_assert **pthis,char *file,unsigned int line,char *left_expr,char *right_expr,int left_result,int right_result) {}
 void tresults_assert_free(tresults_assert **pthis) {}
@@ -106,10 +108,48 @@ void tresults_test_free(tresults_test **pthis) {}
 void tresults_test_add_assert(tresults_test *this, tresults_assert *assert) {}
 void tresults_test_print(tresults_test *this) {}
 
-void tresults_suite_new(tresults_suite **pthis) {}
-void tresults_suite_free(tresults_suite **pthis) {}
+void tresults_suite_new(tsuite *suite,tresults_suite **pthis) {
+    tresults_suite *this=(tresults_suite *)malloc(sizeof(struct tresults_suite));
+
+    this->failed=0;
+    this->suite=suite;
+    this->tests_run=0;
+    this->tests_failed=0;
+    this->asserts_run=0;
+    this->asserts_failed=0;
+    this->tests=test_list_new();
+
+    *pthis = this;
+}
+
+void tresults_suite_free(tresults_suite **pthis) {
+    tresults_suite *this=*pthis;
+
+    test_list_free(this->tests);
+    free(this);
+
+    *pthis=0;
+}
+
 void tresults_suite_add_test(tresults_suite *this,tresults_test *test) {}
-void tresults_suite_print(tresults_suite *this) {}
+
+void tresults_suite_print(tresults_suite *this) {
+    test_list *it;
+    tresults_test *test;
+    unsigned int i;
+
+    printf("    Did tests fail?          %s\n", this->failed ? "YES!@#$" : "no :)");
+    printf("    How many tests   failed? %d of %d\n", this->tests_failed, this->tests_run);
+    printf("    How many asserts failed? %d of %d\n", this->asserts_failed, this->asserts_run);
+    printf("\n");
+
+    for(it=this->tests->next, i=0; it != this->tests; it=it->next, i++) {
+        test=(tresults_test *)it->obj;
+        printf("[%d/%d] %s\n", i+1, this->tests_run, test->test->name);
+        tresults_test_print(test);
+        printf("\n");
+    }
+}
 
 void tresults_new(tresults **pthis) {
     tresults *this=(tresults *)malloc(sizeof(struct tresults));
@@ -121,7 +161,6 @@ void tresults_new(tresults **pthis) {
     this->asserts_run=0;
     this->asserts_failed=0;
     this->suites=test_list_new();
-
 
     *pthis = this;
 }
@@ -142,6 +181,17 @@ static void __tresults_suite_free(tresults_suite *this) {
 
 void tresults_add_suite(tresults *this,tresults_suite *suite) {
     test_list_add(this->suites, suite, (test_list_free_f)__tresults_suite_free);
+
+    if(suite->failed==1) {
+        this->failed=0;
+        this->suites_failed++;
+    }
+
+    this->suites_run++;
+    this->tests_run+=suite->tests_run;
+    this->tests_failed+=suite->tests_failed;
+    this->asserts_run+=suite->asserts_run;
+    this->asserts_failed+=suite->asserts_failed;
 }
 
 void tresults_print(tresults *this) {
@@ -152,18 +202,15 @@ void tresults_print(tresults *this) {
     printf("========================================\n");
     printf("              RESULTS\n");
     printf("\n");
-    printf("    Did tests failed?        %s\n", this->failed ? "YES!@#$" : "no :)");
-    printf("    How many suites failed?  %d of %d\n", this->suites_failed, this->suites_run);
-    printf("    How many suites tests?   %d of %d\n", this->tests_failed, this->tests_run);
-    printf("    How many suites asserts? %d of %d\n", this->asserts_failed, this->asserts_run);
-    printf("\n");
-    printf("************\n");
-    printf("              SUITES\n");
+    printf("    Did tests fail?          %s\n", this->failed ? "YES!@#$" : "no :)");
+    printf("    How many suites  failed? %d of %d\n", this->suites_failed, this->suites_run);
+    printf("    How many tests   failed? %d of %d\n", this->tests_failed, this->tests_run);
+    printf("    How many asserts failed? %d of %d\n", this->asserts_failed, this->asserts_run);
     printf("\n");
 
     for(it=this->suites->next, i=0; it != this->suites; it=it->next, i++) {
         suite=(tresults_suite *)it->obj;
-        printf("[%d/%d] %s\n", suite->suite->name);
+        printf("[%d/%d] %s\n", i+1, this->suites_run,suite->suite->name);
         tresults_suite_print(suite);
         printf("\n");
     }
@@ -223,6 +270,13 @@ void tsuite_add_test(tsuite *this,char *name,ttest_f test) {
     test_list_add(this->tests, atest, (test_list_free_f)__ttest_free);
 }
 
+void tsuite_run(tsuite *this,tresults_suite **presults) {
+    tresults_suite *results;
+    tresults_suite_new(this,&results);
+
+    *presults=results;
+}
+
 static void __tsuite_free(tsuite *this) {
     tsuite_free(&(this));
 }
@@ -250,9 +304,20 @@ void tcontext_add_suite(tcontext *this,tsuite *suite) {
 
 int tcontext_run(tcontext *this) {
     tresults *results=0;
+
+    test_list *suite_it=0;
+    tsuite *suite=0;
+    tresults_suite *results_suite=0;
+
     int ret;
 
     tresults_new(&results);
+
+    for(suite_it=this->suites->next; suite_it!=this->suites; suite_it=suite_it->next) {
+        suite=(tsuite *)suite_it->obj;
+        tsuite_run(suite, &results_suite);
+        tresults_add_suite(results, results_suite);
+    }
 
     ret = results->failed;
 
