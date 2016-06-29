3e0aa546192d64df6dd616e0ae0da3c38a0ee886
tsieprawski <tsieprawski@localhost>
2016-06-29 18:57:43 +0200

    Looks like working.

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ cd build
 mkdir logs
 
 echo "conan..."
-conan install .. &> logs/build-conan.txt || exit 1
+conan install .. &> logs/build-conan.txt || (cat logs/build-conan.txt; exit 1)
 
 echo "cmake..."
-cmake -DCMAKE_BUILD_TYPE=Debug .. &> logs/build-cmake.txt|| exit 1
+cmake -DCMAKE_BUILD_TYPE=Debug .. &> logs/build-cmake.txt|| (cat logs/build-cmake.txt; exit 1)
 
 echo "build..."
-make all &> logs/build-make.txt || exit 1
+make all &> logs/build-make.txt || (cat logs/build-make.txt; exit 1)
 
 echo "OK"
 cd ..
