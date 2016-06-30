272afc8dc715f2215ab0a0f17c964a92618c3db7
tsieprawski <tsieprawski@localhost>
2016-06-30 16:37:44 +0200

    Forgot to remove pythonlibs.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ test -d build || exit 1
 cd build
 
 ctest -VV . &> logs/test-ctest.txt || (cat logs/test-ctest.txt; exit 1)
-xsltproc -o results.xml ../tools/cunit-to-junit.xsl test/CUnitAutomated-Results.xml || exit 1
+# xsltproc -o results.xml ../tools/cunit-to-junit.xsl test/CUnitAutomated-Results.xml || exit 1
 
 echo "OK!"
 cd ..
