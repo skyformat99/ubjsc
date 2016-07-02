fc06705d322c4446e9a0c12221df9a004bd4a681
tsieprawski <tsieprawski@localhost>
2016-07-02 14:43:10 +0200

    More issues fixed.

--- src/ubjs_common.c
+++ src/ubjs_common.c
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2016 Tomasz Sieprawski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,8 +28,10 @@
 
 ubjs_endian_host_type __ubjs_endian_forced=UEFT_DEFAULT;
 
-ubjs_result ubjs_endian_host_type_get(ubjs_endian_host_type *type) {
-    if(0 == type) {
+ubjs_result ubjs_endian_host_type_get(ubjs_endian_host_type *type)
+{
+    if (0 == type)
+    {
         return UR_ERROR;
     }
 
@@ -15,8 +39,10 @@ ubjs_result ubjs_endian_host_type_get(ubjs_endian_host_type *type) {
     return UR_OK;
 }
 
-ubjs_result ubjs_endian_host_type_set(ubjs_endian_host_type type) {
-    switch(type) {
+ubjs_result ubjs_endian_host_type_set(ubjs_endian_host_type type)
+{
+    switch (type)
+    {
     case UEFT_DEFAULT:
     case UEFT_LITTLE:
     case UEFT_BIG:
@@ -27,10 +53,12 @@ ubjs_result ubjs_endian_host_type_set(ubjs_endian_host_type type) {
     return UR_ERROR;
 }
 
-ubjs_result ubjs_endian_is_big(ubjs_bool *ret) {
+ubjs_result ubjs_endian_is_big(ubjs_bool *ret)
+{
     volatile uint32_t i=0x01234567;
 
-    switch(__ubjs_endian_forced){
+    switch (__ubjs_endian_forced)
+    {
     case UEFT_LITTLE:
         (*ret)=UFALSE;
         break;
@@ -43,47 +71,60 @@ ubjs_result ubjs_endian_is_big(ubjs_bool *ret) {
     return UR_OK;
 }
 
-static void swap(uint8_t *in,uint8_t *out,int len) {
+static void swap(uint8_t *in, uint8_t *out, int len)
+{
     int i;
 
-    for(i=0; len>i; i++) {
+    for (i=0; len>i; i++)
+    {
         out[len - i - 1] = in[i];
     }
 }
 
-static void copy(uint8_t *in,uint8_t *out,int len) {
+static void copy(uint8_t *in, uint8_t *out, int len)
+{
     int i;
 
-    for(i=0; len>i; i++) {
+    for (i=0; len>i; i++)
+    {
         out[i] = in[i];
     }
 }
 
-ubjs_result ubjs_endian_convert_big_to_native(uint8_t *in,uint8_t *out,int len) {
+ubjs_result ubjs_endian_convert_big_to_native(uint8_t *in, uint8_t *out, int len)
+{
     ubjs_bool big;
     ubjs_endian_is_big(&big);
 
-    if(UTRUE == big) {
+    if (UTRUE == big)
+    {
         copy(in, out, len);
-    } else {
+    }
+    else
+    {
         swap(in, out, len);
     }
     return UR_OK;
 }
 
-ubjs_result ubjs_endian_convert_native_to_big(uint8_t *in,uint8_t *out,int len) {
+ubjs_result ubjs_endian_convert_native_to_big(uint8_t *in, uint8_t *out, int len)
+{
     ubjs_bool big;
     ubjs_endian_is_big(&big);
 
-    if(UTRUE == big) {
+    if (UTRUE == big)
+    {
         copy(in, out, len);
-    } else {
+    }
+    else
+    {
         swap(in, out, len);
     }
     return UR_OK;
 }
 
-ubjs_result ubjs_compact_sprintf(char **pthis, unsigned int *this_len, char *format, ...) {
+ubjs_result ubjs_compact_sprintf(char **pthis, unsigned int *this_len, char *format, ...)
+{
     char *now = 0;
     int ret;
     int length;
@@ -104,4 +145,3 @@ ubjs_result ubjs_compact_sprintf(char **pthis, unsigned int *this_len, char *for
     *pthis=now;
     return UR_OK;
 }
-
