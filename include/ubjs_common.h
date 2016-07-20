f54dad871a891955215c1c0bb25d742c4e10709c
tsieprawski <tsieprawski@localhost>
2016-07-20 20:38:55 +0200

    First documentation.

--- include/ubjs_common.h
+++ include/ubjs_common.h
@@ -19,7 +19,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  **/
-
+/*! \file
+ *  \brief Definitions of common types, ubjson markers and some utilities.
+ */
+ 
 #ifndef HAVE_UBJS_COMMON
 #define HAVE_UBJS_COMMON
 
@@ -31,46 +34,86 @@ extern "C"
 #include <stdint.h>
 #include "ubjs_exports.h"
 
+
+/*! \brief Typedef to have consistency in naming all int/float types.
+ */
 typedef float float32_t;
+
+/*! \brief Typedef to have consistency in naming all int/float types.
+ */
 typedef double float64_t;
 
+/*! \brief Wrapper around regular C-like booleans.
+ *
+ * Created for consistency and readibility.
+ */
 typedef enum ubjs_bool
 {
-    UFALSE,
-    UTRUE
+    UFALSE, /*!< False. */
+    UTRUE /*!< True. */
 } ubjs_bool;
 
+/*! \brief Enum that defines the result of a method.
+ *
+ * About 99% methods in ubjs API return ubjs_result, with every output being passed
+ * by parameters via pointers. This way every method easily tells whether its operation
+ * succeded.
+ */
 typedef enum ubjs_result
 {
-    UR_OK,
-    UR_ERROR
+    UR_OK, /*!< Operation succeded competely. */
+    UR_ERROR /*!< Invalid input or otherwise errorness result. */
 } ubjs_result;
 
+/*! \brief Enum that defines the endiannes of this platform.
+ *
+ * Ubjson numbers need to be parsed/written in big endian, and thus we need a way to know
+ * whether we need to convert from our little endian platform or not.
+ */
 typedef enum ubjs_endian_host_type
 {
-    UEFT_DEFAULT,
-    UEFT_LITTLE,
-    UEFT_BIG
+    UEFT_DEFAULT, /*!< Default endianness. Aka you do not care which one it is. */
+    UEFT_LITTLE, /*!< Little endian - conversion needed. */
+    UEFT_BIG /*!< Big endian. */
 } ubjs_endian_host_type;
 
+/*! \brief Marker "#" that precedes length of a optimized container. */
 #define MARKER_OPTIMIZE_COUNT 35
+/*! \brief Marker "$" that precedes type of a optimized container. */
 #define MARKER_OPTIMIZE_TYPE 36
+/*! \brief Marker "C" that precedes a character. */
 #define MARKER_CHAR 67
+/*! \brief Marker "D" that precedes a float64 number. */
 #define MARKER_FLOAT64 68
+/*! \brief Marker "F" that signals a false value. */
 #define MARKER_FALSE 70
+/*! \brief Marker "I" that precedes an int16 number. */
 #define MARKER_INT16 73
+/*! \brief Marker "L" that precedes an int64 number. */
 #define MARKER_INT64 76
+/*! \brief Marker "N" that signals a no-operation. */
 #define MARKER_NOOP 78
+/*! \brief Marker "S" that precedes a string. */
 #define MARKER_STR 83
+/*! \brief Marker "T" that signals a true value. */
 #define MARKER_TRUE 84
+/*! \brief Marker "L" that precedes an uint8 number. */
 #define MARKER_UINT8 85
+/*! \brief Marker "Z" that signals a null value. */
 #define MARKER_NULL 90
+/*! \brief Marker "[" that signals beginning of an array. */
 #define MARKER_ARRAY_BEGIN 91
+/*! \brief Marker "]" that signals end of an unoptimized array. */
 #define MARKER_ARRAY_END 93
+/*! \brief Marker "d" that precedes a float32 number. */
 #define MARKER_FLOAT32 100
+/*! \brief Marker "i" that precedes an int8 number. */
 #define MARKER_INT8 105
+/*! \brief Marker "l" that precedes an int32 number. */
 #define MARKER_INT32 108
+/*! \brief Marker "[" that signals beginning of an object. */
 #define MARKER_OBJECT_BEGIN 123
+/*! \brief Marker "]" that signals end of an unoptimized object. */
 #define MARKER_OBJECT_END 125
 
 UBJS_EXPORT ubjs_result ubjs_endian_is_big(ubjs_bool *);
