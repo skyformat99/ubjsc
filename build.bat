17a264e337e5fc3c105c75a1c89dec5ee972899b
tsieprawski <tsieprawski@localhost>
2016-07-20 15:45:57 +0200

    Better build scripts.

--- build.bat
+++ build.bat
@@ -1,12 +1,18 @@
 echo '***** BUILD *****'
 
-rmdir /s /q build2
+rmdir /s /q build
 
-mkdir build2
-cd build2
+mkdir build
+cd build
 mkdir logs
 
 "C:\Program Files\CMake\bin\cmake" ..
+if %errorlevel% neq 0 exit /b 1
+
 "C:\Program Files\CMake\bin\cmake" --build .
+if %errorlevel% neq 0 exit /b 1
+
+"C:\Program Files\CMake\bin\ctest" -VV .
+if %errorlevel% neq 0 exit /b 1
 
 cd ..
