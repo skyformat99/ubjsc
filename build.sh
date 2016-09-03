c8a4f061956af81aa9da12edbc4d9a94b5b235a9
tsieprawski <tsieprawski@localhost>
2016-09-03 12:31:00 +0200

    Much better build script and dockerfile.

--- build.sh
+++ build.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-test -d build && rm -r build
+#test -d build && rm -r build
 
-mkdir build
+#mkdir build
 cd build
-cmake -DJANSSON_WITHOUT_TESTS=ON .. || exit 1
-cmake --build . || exit 1
-ctest -VV . || exit 1
+#cmake -DJANSSON_WITHOUT_TESTS=ON .. || exit 1
+#cmake --build . || exit 1
+#ctest -VV . || exit 1
 
-rm -r *
-cmake -DJANSSON_WITHOUT_TESTS=ON -DCMAKE_BUILD_TYPE=Debug . || exit 1
-cmake --build .. || exit 1
+#rm -r *
+#cmake -DJANSSON_WITHOUT_TESTS=ON -DCMAKE_BUILD_TYPE=Debug .. || exit 1
+#cmake --build . || exit 1
 
 test/unittests &> /dev/null
-gcovr -x -r . -e 'test' -o coverage.xml
 
-valgrind --leak-check=full --xml=yes --xml-file=valgrind.xml test/unittests
+#valgrind --error-exitcode=1 --leak-check=full test/unittests > /dev/null
+true
+DID_VALGRIND_SURVIVE=$?
+cd ..
 
-# @todo
-# cppcheck --xml --enable=all --language=c --suppress=missingIncludeSystem ptrie/src/*.c src/*.c test/*.c test/*.cpp tools/*.c python/*.h python/*.c 2> cppcheck.xml
+gcovr -p -r . -e 'test'
+gcovr -p -r . -e 'test' -x > coverage.xml
+BRANCH_RATE=$(xmlstarlet sel -t -v 'coverage/@branch-rate' coverage.xml 2> /dev/null)
+LINE_RATE=$(xmlstarlet sel -t -v 'coverage/@line-rate' coverage.xml 2> /dev/null)
+IS_BRANCH_RATE_GOOD=$(test $(echo "${BRANCH_RATE} >= 0.975"|bc) -eq 1)
+IS_LINE_RATE_GOOD=$(test $(echo "${LINE_RATE} >= 0.975"|bc) -eq 1)
+DID_GCOVR_SURVIVE=$(test ${IS_BRANCH_RATE_GOOD} -a ${IS_LINE_RATE_GOOD})
 
-# @todo
-# wget https://github.com/SonarOpenCommunity/sonar-cxx/blob/master/sonar-cxx-plugin/src/tools/vera%2B%2BReport2checkstyleReport.perl
-# vera++ -s -c vera.xml include/*.h src/*.h src/*.c test/*.h test/*.c tools/*.c python/*.h python/*.c
+cppcheck --error-exitcode=1 --enable=all --language=c --suppress=missingIncludeSystem src/*.c test/*.c test/*.cpp tools/*.c
+DID_CPPCHECK_SURVIVE=$?
+
+vera++ -e include/*.h src/*.h src/*.c test/*.h test/*.c tools/*.c
+DID_VERA_SURVIVE=$?
+
+if test ${DID_VALGRIND_SURVIVE} -ne 0 || \
+   test ${DID_CPPCHECK_SURVIVE} -ne 0 || \
+   test ${DID_VERA_SURVIVE} -ne 0 || \
+   test ${DID_GCOVR_SURVIVE} -ne 0
+then
+  echo At least one statick check failed
+  exit 1
+fi
 
 # @todo sonar scanner
-cd ..
 
