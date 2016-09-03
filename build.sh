ca0f08b0813a1f6d798da13b36b2cb89680a1732
tsieprawski <tsieprawski@localhost>
2016-09-03 10:36:33 +0200

    Fixes. Copied flow from bamboo.

--- build.sh
+++ build.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
-echo '***** BUILD *****'
-
-test -d build && rm -rf build
+test -d build && rm -r build
 
 mkdir build
 cd build
+cmake -DJANSSON_WITHOUT_TESTS=ON .. || exit 1
+cmake --build . || exit 1
+ctest -VV . || exit 1
 
-if ! cmake -DCMAKE_BUILD_TYPE=Debug ..
-then
-    exit 1
-fi
+rm -r *
+cmake -DJANSSON_WITHOUT_TESTS=ON -DCMAKE_BUILD_TYPE=Debug . || exit 1
+cmake --build .. || exit 1
 
-if ! cmake --build .
-then
-    exit 1
-fi
+test/unittests &> /dev/null
+gcovr -x -r . -e 'test' -o coverage.xml
 
-if ! ctest -VV .
-then
-    exit 1
-fi
+valgrind --leak-check=full --xml=yes --xml-file=valgrind.xml test/unittests
 
+# @todo
+# cppcheck --xml --enable=all --language=c --suppress=missingIncludeSystem ptrie/src/*.c src/*.c test/*.c test/*.cpp tools/*.c python/*.h python/*.c 2> cppcheck.xml
+
+# @todo
+# wget https://github.com/SonarOpenCommunity/sonar-cxx/blob/master/sonar-cxx-plugin/src/tools/vera%2B%2BReport2checkstyleReport.perl
+# vera++ -s -c vera.xml include/*.h src/*.h src/*.c test/*.h test/*.c tools/*.c python/*.h python/*.c
+
+# @todo sonar scanner
 cd ..
+
