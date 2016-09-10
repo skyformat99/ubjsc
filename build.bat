7191d9964b54e2be7ad6900a5aef0f80c41b6b1d
tsieprawski <tsieprawski@localhost>
2016-09-10 14:41:17 +0200

    Maybe this one.

--- build.bat
+++ build.bat
@@ -3,8 +3,8 @@ rmdir /s /q build
 mkdir build
 cd build
 
-curl -o ubjsc-0.4-deps.7z "http://dl.dropboxusercontent.com/s/6l849mulq4vzv08/ubjsc-0.4-deps.7z"
-7z x ubjsc-0.4-deps.7z
+curl -o ubjsc-0.4-deps.zip "http://dl.dropboxusercontent.com/s/23utlhfmo3afh35/ubjsc-0.4-deps.zip?dl=0#
+7z x ubjsc-0.4-deps.zip
 
 cmake -DJANSSON_EXPORT_DIR=.\jansson -DARGTABLE2_EXPORT_DIR=.\argtable2 ..
 if %errorlevel% neq 0 exit /b 1
