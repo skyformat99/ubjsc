45f953e1d175eaadb3d4f8e17caa805e7eeea307
tsieprawski <tsieprawski@localhost>
2016-11-04 20:25:00 +0100

    Coverage and stuff.

--- check-static.sh
+++ check-static.sh
@@ -2,24 +2,24 @@
 set -x
 
 FAILED=0
+HEADERS_C=$(find include test src -name '*.h')
+SOURCES_C=$(find include test src -name '*.c')
+SOURCES_PY=$(find python -name '*.py')
 
 cppcheck --error-exitcode=1 --enable=all --language=c \
-    --suppress=missingIncludeSystem \
-    $(find include test src -name '*.h') \
-    $(find include test src -name '*.c') \
+    --suppress=missingIncludeSystem ${SOURCES_C} \
 || FAILED=1
 
 vera++ -e \
-    $(find include test src -name '*.h') \
-    $(find include test src -name '*.c') \
+    ${HEADERS_C} ${SOURCES_C} \
 || FAILED=1
 
-pep8 --max-line-length=100 \
-    $(find python -name '*.py') \
+pep8 --max-line-length=100
+    ${SOURCES_PY} \
 || FAILED=1
 
 pylint \
-    $(find python -name '*.py') \
+    ${SOURCES_PY} \
 || FAILED=1
 
 (
@@ -29,3 +29,5 @@ pylint \
     cmake ..
     make man html
 ) || FAILED=1
+
+exit $FAILED
