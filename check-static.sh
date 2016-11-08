50921d020a066b1fc634a893e7dd1c79d149292b
tsieprawski <tsieprawski@localhost>
2016-11-08 19:53:41 +0100

    Fixed cpack stuff. Another docker image, and stuff.

--- check-static.sh
+++ check-static.sh
@@ -6,20 +6,28 @@ HEADERS_C=$(find include test src -name '*.h')
 SOURCES_C=$(find include test src -name '*.c')
 SOURCES_PY=$(find python -name '*.py')
 
+# shellcheck disable=SC2086
 cppcheck --error-exitcode=1 --enable=all --language=c \
     --suppress=missingIncludeSystem ${SOURCES_C} \
 || FAILED=1
 
+# shellcheck disable=SC2086
 vera++ -e \
     ${HEADERS_C} ${SOURCES_C} \
 || FAILED=1
 
+# shellcheck disable=SC2086
 pep8 --max-line-length=100 \
     ${SOURCES_PY} \
 || FAILED=1
 
+# shellcheck disable=SC2086
 pylint \
     ${SOURCES_PY} \
+|| FAILED=1
+
+# shellcheck disable=SC2086
+shellcheck ./*.sh \
 || FAILED=1
 
 (
