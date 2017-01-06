a22cb26b6262042b5b36e920e2be4a1a091f466c
tsieprawski <tsieprawski@localhost>
2017-01-06 12:32:04 +0100

    Refactored library builder to allow for many build()s. Maybe someone in this world will use it?

--- check-leaks.sh
+++ check-leaks.sh
@@ -5,13 +5,18 @@ test -d build && rm -r build
 mkdir build
 cd build || exit 1
 cmake -DCMAKE_BUILD_TYPE=Debug .. &>/dev/null || exit 1
-make test-ubjsc test-ubjsc-glue-dict-ptrie ubjspy &>/dev/null || exit 1
+make test-ubjsc test-ubjsc-glues test-ubjsc-glue-dict-ptrie ubjspy &>/dev/null || exit 1
 
 # Yeah, we really use this much memory.
 FAILED=0
 valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all --show-reachable=yes \
     --suppressions=../valgrind.supp --gen-suppressions=all \
     ./test-ubjsc > /dev/null
+test $? == 1 && FAILED=1
+
+valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all --show-reachable=yes \
+    --suppressions=../valgrind.supp --gen-suppressions=all \
+    ./test-ubjsc-glues > /dev/null
 test $? == 1 && FAILED=1
 
 valgrind --error-exitcode=1 --leak-check=full --show-leak-kinds=all --show-reachable=yes \
