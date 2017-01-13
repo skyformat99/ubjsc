8b7b7b8ce103ce249c91dac086ea3320fc766c8a
tsieprawski <tsieprawski@localhost>
2017-01-13 23:53:00 +0100

    Bumping version.

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ mkdir dist
 cd build || exit 1
 cmake -DCPACK_GENERATOR="TBZ2;ZIP;DEB" -DCPACK_SOURCE_GENERATOR="TBZ2;ZIP;DEB" ..
 make html man package package_source
-mv libubjsc0.4-*.{tar.bz2,zip,deb} ../dist
+mv libubjsc0.5-*.{tar.bz2,zip,deb} ../dist
 cd ../dist || exit 1
 
 du -ah
