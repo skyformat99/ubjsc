1aa64bf2a87d0a12c09d4fa03de7ba214d3e30d1
tsieprawski <tsieprawski@localhost>
2017-01-17 02:56:00 +0100

    HTTP server.

--- tools/artifact-server/server.py
+++ tools/artifact-server/server.py
@@ -6,18 +6,31 @@ import os
 import os.path
 import imp
 import sys
+import threading
+from http.server import SimpleHTTPRequestHandler
+import socketserver
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import TLS_FTPHandler
 from pyftpdlib.servers import FTPServer
 
-if __name__ == '__main__':
-    config = imp.load_source('config', sys.argv[1])
+DATA = 'files'
 
-    data = os.path.join(os.getcwd(), 'files')
+
+class HS(SimpleHTTPRequestHandler):
+
+    def translate_path(self, path):
+        return os.sep.join([DATA, path])
+
+
+def run_http_server(config):
+    socketserver.TCPServer.allow_reuse_address = True
+    httpd = socketserver.TCPServer(('', 2180), HS)
+    httpd.serve_forever()
+
+
+def run_ftps_server(config):
     authorizer = DummyAuthorizer()
-
-    authorizer.add_user('pipelines', config.PASSWD, data, perm='elrmw')
-    authorizer.add_anonymous(data)
+    authorizer.add_user('pipelines', config.PASSWD, DATA, perm='elrmw')
 
     handler = TLS_FTPHandler
     handler.certfile = config.PEMFILE
@@ -29,3 +42,17 @@ if __name__ == '__main__':
     server = FTPServer(address, handler)
 
     server.serve_forever()
+
+if __name__ == '__main__':
+    config = imp.load_source('config', sys.argv[1])
+
+    ftp = threading.Thread(target=run_ftps_server, args=(config,))
+    ftp.daemon = True
+    ftp.start()
+
+    http = threading.Thread(target=run_http_server, args=(config,))
+    http.daemon = True
+    http.start()
+
+    ftp.join()
+    http.join()
