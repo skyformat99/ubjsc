2c082df538065c16b166940a89508af6413adb7d
tsieprawski <tsieprawski@localhost>
2017-01-31 02:15:00 +0100

    Better upload script.

--- upload_artifacts.py
+++ upload_artifacts.py
@@ -18,33 +18,54 @@ if 'ARTIFACT_SERVER_URL' not in os.environ:
 URL = urllib.parse.urlparse(os.environ['ARTIFACT_SERVER_URL'])
 COMMIT = os.environ['BITBUCKET_COMMIT']
 
-with FTP_TLS() as cli:
+
+def main(cli):
+    """
+        @todo
+    """
+
     cli.connect(host=URL.hostname, port=URL.port)
     cli.auth()
     cli.prot_p()
     cli.login(URL.username, URL.password)
 
-    try:
-        cli.mkd(COMMIT)
-    except all_errors:
-        traceback.print_exc()
+    tasks = []
 
-    for root, dirs, files in os.walk(DATA):
+    def do_tha_dir(adir):
+        """
+            @todo
+        """
+        print("MKDIR {}".format(adir))
+        cli.mkd(adir)
+
+    def do_tha_file(afrom, ato):
+        """
+            @todo
+        """
+        print("PUT {}".format(ato))
+        with open(afrom, 'rb') as afile:
+            cli.storbinary("STOR {}".format(ato), afile)
+
+    tasks.append((do_tha_dir, COMMIT))
+
+    for root, _unused, files in os.walk(DATA):
+        _unused = _unused
+
         root_cut = root[len(DATA) + 1:]
         target_dir_path = os.sep.join([COMMIT, root_cut])
-        try:
-            cli.mkd(target_dir_path)
-        except all_errors:
-            traceback.print_exc()
+        tasks.append((do_tha_dir, target_dir_path))
 
         for file in files:
             local_path = os.sep.join([root, file])
             target_path = os.sep.join([COMMIT, root_cut, file])
+            tasks.append((do_tha_file, local_path, target_path))
 
-            print(local_path)
+    for task in tasks:
+        try:
+            func, *args = task
+            func(*args)
+        except all_errors:
+            traceback.print_exc(file=sys.stdout)
 
-            try:
-                with open(local_path, 'rb') as fp:
-                    cli.storbinary("STOR {}".format(target_path), fp)
-            except all_errors:
-                traceback.print_exc()
+with FTP_TLS() as acli:
+    main(acli)
