58a0294b0e3060ac374e0d7920e9507c31449f8d
tsieprawski <tsieprawski@localhost>
2017-02-04 15:21:17 +0100

    Profile tests with callgrind and print summaries in CI.

---
+++ check-profile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -x
+
+test -d build && rm -r build
+mkdir build
+cd build || exit 1
+cmake -DCMAKE_BUILD_TYPE=Debug .. &>/dev/null || exit 1
+make test-ubjsc test-ubjsc-glues test-ubjsc-glue-dict-ptrie &>/dev/null || exit 1
+
+valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc.out \
+    ./test-ubjsc > /dev/null
+callgrind_annotate callgrind.ubjsc.out
+
+valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc-glues.out \
+    ./test-ubjsc-glues > /dev/null
+callgrind_annotate callgrind.ubjsc-glues.out
+
+valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc-glue-dict-ptrie.out \
+    ./test-ubjsc-glue-dict-ptrie > /dev/null
+callgrind_annotate callgrind.ubjsc-glue-dict-ptrie.out
