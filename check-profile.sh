a959c21811abcb925bff572e33d8c7050bdb2289
tsieprawski <tsieprawski@localhost>
2017-03-14 21:30:00 +0100

    Callgrind will now output and trace children of the process. Preparing for backmerge.

--- check-profile.sh
+++ check-profile.sh
@@ -7,14 +7,14 @@ cd build || exit 1
 cmake -DCMAKE_BUILD_TYPE=Debug .. &>/dev/null || exit 1
 make test-ubjsc test-ubjsc-glues test-ubjsc-glue-dict-ptrie &>/dev/null || exit 1
 
-valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc.out \
+valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc.%p.out --trace-children=yes \
     ./test-ubjsc > /dev/null
-callgrind_annotate callgrind.ubjsc.out
+callgrind_annotate callgrind.ubjsc.*.out
 
-valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc-glues.out \
+valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc-glues.%p.out --trace-children=yes \
     ./test-ubjsc-glues > /dev/null
-callgrind_annotate callgrind.ubjsc-glues.out
+callgrind_annotate callgrind.ubjsc-glues.*.out
 
-valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc-glue-dict-ptrie.out \
+valgrind --tool=callgrind --callgrind-out-file=callgrind.ubjsc-glue-dict-ptrie.%p.out --trace-children=yes \
     ./test-ubjsc-glue-dict-ptrie > /dev/null
-callgrind_annotate callgrind.ubjsc-glue-dict-ptrie.out
+callgrind_annotate callgrind.ubjsc-glue-dict-ptrie.*.out
