531653ecdde944ee08d4230d5577ba95f9517d34
tsieprawski <tsieprawski@localhost>
2017-03-27 22:25:00 +0200

    Introducing delete for dict iterator, to free items early.

--- check-profile.sh
+++ check-profile.sh
@@ -7,10 +7,12 @@ cd build || exit 1
 cmake -DCMAKE_BUILD_TYPE=Debug -DWITH_TESTING=ON .. &>/dev/null || exit 1
 make test-api-ubjsc &>/dev/null || exit 1
 
-valgrind --tool=callgrind --callgrind-out-file=callgrind-%p.ubjsc.out --trace-children=yes \
+valgrind --tool=callgrind --callgrind-out-file=callgrind-%p.out --trace-children=yes \
     ./test-api-ubjsc --verbose > /dev/null
-valgrind --tool=callgrind --callgrind-out-file=callgrind-%p.ubjsc-glue.out --trace-children=yes \
+valgrind --tool=callgrind --callgrind-out-file=callgrind-%p.out --trace-children=yes \
     ./test-api-ubjsc-glue --verbose > /dev/null
+valgrind --tool=callgrind --callgrind-out-file=callgrind-%p.out --trace-children=yes \
+    ./test-api-ubjsc-glue-dict-ptrie --verbose > /dev/null
 for afile in callgrind-*.ubjsc.out
 do
     callgrind_annotate "${afile}"
