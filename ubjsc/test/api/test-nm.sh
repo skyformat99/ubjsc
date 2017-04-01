7d1963a6ecb2ef0e3254d261c4309d708edf48bd
tsieprawski <tsieprawski@localhost>
2017-04-01 02:20:00 +0200

    Added simiar test for ptrie glue. Renamed stuff.

--- ubjsc/test/api/test-nm.sh
+++ ubjsc/test/api/test-nm.sh
@@ -5,7 +5,7 @@ HERE=$(dirname "$0")
 FAIL=0
 
 nm -g "./libubjsc.so" | grep 'T ubjs' | sed 's/^[0-9a-f]* T //' &> tested.txt
-diff "${HERE}/libubjsc-nm.txt" tested.txt
+diff "${HERE}/lib-nm.txt" tested.txt
 test $? -eq 0 || FAIL=1
 rm tested.txt
 
