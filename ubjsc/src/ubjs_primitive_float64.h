87ef6a1ab09da40618aa21cd4828e1c116067d39
tsieprawski <tsieprawski@localhost>
2017-05-05 19:13:00 +0200

    Forgot to add files.

---
+++ ubjsc/src/ubjs_primitive_float64.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2016 Tomasz Sieprawski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ **/
+/*! \file
+ *  \brief No-operation.
+ *
+ *  ubjson.org says that "when parsed by the receiver, the no-op valu$
+ *  and carry know meaningful value with them.". I've decided to leav$
+ *  and because of that, no-ops are parsed always and passed to the u$
+ *
+ *  For ubjsc these two arrays are NOT equal on the parse/write level:
+ *
+ *  - ["foo", "bar", "baz"]
+ *  - ["foo", no-op, "bar", no-op, no-op, no-op, "baz", no-op, no-op]
+ * \since 0.7
+ */
+
+#ifndef HAVE_UBJS_PRIMITIVE_FLOAT64
+#define HAVE_UBJS_PRIMITIVE_FLOAT64
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <ubjs_library.h>
+#include <ubjs_primitives.h>
+
+/*!
+ * \since 0.7
+ */
+UBJS_EXPORT extern ubjs_prmtv_ntype ubjs_prmtv_float64_ntype;
+
+/*! \brief Returns int8 primitive.
+ *
+ * This is a singleton and ubj_prmtv_free do nothing.
+ */
+UBJS_EXPORT ubjs_result ubjs_prmtv_float64(ubjs_library *, float64_t,
+    ubjs_prmtv **);
+
+/*!
+ * \since 0.7
+ */
+UBJS_EXPORT ubjs_result ubjs_prmtv_float64_get(ubjs_prmtv *, float64_t *);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
