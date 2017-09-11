4696d6cc7a213346cbe5a121bf6569a707a6a811
tsieprawski <tsieprawski@localhost>
2017-09-11 23:29:00 +0200

    Better valgrind integration within cmake files. Runs exactly the same tests like ctest - we have bonus tools coverage.

--- check-leaks.sh
+++ check-leaks.sh
@@ -4,18 +4,11 @@ set -x
 test -d build && rm -r build
 mkdir build
 cd build || exit 1
-cmake -DWITH_TESTING=ON -DCMAKE_BUILD_TYPE=Debug .. || exit 1
+cmake -DWITH_TESTING=ON -DCMAKE_BUILD_TYPE=Debug -DWITH_VALGRIND=ON .. || exit 1
 make || exit 1
 
-valgrind --log-file=memcheck.%p.txt --leak-check=full --show-leak-kinds=all --show-reachable=yes \
-    --trace-children=yes \
-    ./test-api-ubjsc --verbose --jobs 0
-valgrind --log-file=memcheck.%p.txt --leak-check=full --show-leak-kinds=all --show-reachable=yes \
-    --trace-children=yes \
-    ./test-api-ubjsc-glue --verbose --jobs 0
-valgrind --log-file=memcheck.%p.txt --leak-check=full --show-leak-kinds=all --show-reachable=yes \
-    --trace-children=yes \
-    ./test-api-ubjsc-glue-dict-ptrie --verbose --jobs 0
+ctest -VV .
+
 NUM_OF_FILES=$(find . -maxdepth 1 -type f -name 'memcheck.*.txt' | wc -l)
 NUM_OF_PASSED=$(find . -maxdepth 1 -type f -name 'memcheck.*.txt' \
     -exec bash -c "grep 'ERROR SUMMARY: 0 errors from 0 contexts' >/dev/null \$1 && echo \$1" _ {} \;| wc -l)
@@ -27,12 +20,3 @@ then
         _ {} \;
     exit 1
 fi
-
-# @todo this involves custom-built python
-#(
-#  cd python
-#  valgrind --error-exitcode=1 --suppressions=/usr/lib/valgrind/python3.supp \
-#    --leak-check=full \
-#    python3 -X showrefcount ../../python/setup.py test > /dev/null \
-#) \
-#|| FAILED=1
