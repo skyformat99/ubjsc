cb3be57a799d50ec8173959b1b619ebb6d1c78b9
tsieprawski <tsieprawski@localhost>
2017-09-18 21:37:00 +0200

    Static fixes.

--- print-leaks.sh
+++ print-leaks.sh
@@ -2,7 +2,7 @@
 set -x
 
 test -d build || exit 1
-cd build
+cd build || exit
 find . -maxdepth 1 -type f -name 'memcheck.*.txt' \
     -exec bash -c "grep 'ERROR SUMMARY: 0 errors from 0 contexts' \$1 >/dev/null || (echo \$1;cat \$1;echo)" \
     _ {} \;
