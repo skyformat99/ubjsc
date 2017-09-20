d7f408cf9df92bf89b122b5f8d237182cedf0ded
tsieprawski <tsieprawski@localhost>
2017-09-20 05:02:00 +0200

    Another try.

--- check-static-complexity.sh
+++ check-static-complexity.sh
@@ -3,4 +3,8 @@ SOURCES_NOTEST_C=$(find tools ubjsc/src ubjsc-glue-dict-ptrie/src -name '*.c')
 
 # shellcheck disable=SC2086
 complexity --scores --histogram --threshold=13 \
-    ${SOURCES_NOTEST_C} && exit 1
+    ${SOURCES_NOTEST_C} | tee a.tmp
+grep 'No procedures were scored' a.tmp &> /dev/null
+EXIT=$?
+rm a.tmp
+exit ${EXIT}
