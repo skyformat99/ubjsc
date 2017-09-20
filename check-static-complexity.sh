5f41aac70d7f5f16d8bef2ce145ff5f7e240aa2c
tsieprawski <tsieprawski@localhost>
2017-09-20 02:00:00 +0200

    Reverted complexity threshold. Tests in CI do not need to be verbose. Suddenly this test started to fail.

--- check-static-complexity.sh
+++ check-static-complexity.sh
@@ -2,5 +2,5 @@
 SOURCES_NOTEST_C=$(find tools ubjsc/src ubjsc-glue-dict-ptrie/src -name '*.c')
 
 # shellcheck disable=SC2086
-complexity --scores --histogram --threshold=10 \
+complexity --scores --histogram --threshold=13 \
     ${SOURCES_NOTEST_C} && exit 1
