03bdb53513986b17fc5eb74c0dd743eedb7b355f
tsieprawski <tsieprawski@localhost>
2017-09-21 23:47:00 +0200

    Hunterization. Argtable2 is now used by hunter.

--- ubjsc-glue-dict-ptrie/test/test-nm.sh
+++ ubjsc-glue-dict-ptrie/test/test-nm.sh
@@ -3,8 +3,7 @@ set -x
 
 HERE=$(dirname "$0")
 FAIL=0
-
-nm -g "./libubjsc-glue-dict-ptrie.so" | grep 'T ubjs' | sed 's/^[0-9a-f]* T //' &> tested.txt
+nm -g ./libubjsc-glue-dict-ptrie*.so | grep 'T ubjs' | sed 's/^[0-9a-f]* T //' &> tested.txt
 diff "${HERE}/lib-nm.txt" tested.txt
 test $? -eq 0 || FAIL=1
 rm tested.txt
