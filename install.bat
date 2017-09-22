be69c442eec01e333ea2911745a50a0a65fb329d
tsieprawski <tsieprawski@localhost>
2017-09-22 23:39:00 +0200

    Restart of work.

--- install.bat
+++
@@ -1,6 +0,0 @@
-set CYGWIN_ROOT=C:/cygwin
-set CYGWIN_CACHE=C:/cygwin/var/cache/setup
-
-appveyor DownloadFile http://cygwin.com/setup-x86.exe -FileName setup.exe
-setup.exe -dN -R "%CYGWIN_ROOT%" -l "%CYGWIN_CACHE%" -P make
-%CYGWIN_ROOT%/bin/bash -lc "cygcheck -dc cygwin"
